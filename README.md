590d14fead16b0a633ca85b0cf2cff2f162b7b1d
Jack Harbon <jacek@harbon.uk>
2022-12-10 00:47:13 +0500

    PATCH single article votes, styling

--- README.md
+++ README.md
@@ -5,3 +5,51 @@
 [Elephant](postgres://cceesolo:t55oRgsK9CQZXDYjn9sbPy7m8eTlZH7T@mouse.db.elephantsql.com/cceesolo)
 
 [Cyclic](https://be-nc-news-img.cyclic.app/api)
+
+[GitHub](https://github.com/jackharbon/fe-nc-news-img)
+
+# Features
+
+1. Dark and light theme
+
+2. Home page with hero header
+
+3. Stiky navbar (top menu) and sidebar (topics)
+
+4. Responsive navbar: 768px moves to the top
+
+5. Responsive layout: 540px top menu and topics go to the hamburger menu, articles in one column
+
+6. Button 'Show comments' opens comments
+
+7. PATCH votes for single article
+
+# Screenshots
+
+![1 home light](/1-home-light.jpg '1 home light')
+
+![1 home dark](/1-home-dark.jpg '1 home dark')
+
+![2-stiky-bars-light](/2-stiky-bars-light.jpg '2-stiky-bars-light')
+
+![2-stiky-bars-dark](/2-stiky-bars-dark.jpg '2-stiky-bars-dark')
+
+![3-sidebar-top-light](/3-sidebar-top-light.jpg '3-sidebar-top-light')
+
+![4-burger-menu-light](/4-burger-menu-light.jpg '4-burger-menu-light')
+
+![4-burger-menu-dark](/4-burger-menu-dark.jpg '4-burger-menu-dark')
+
+![5-burger-menu-open-light](/5-burger-menu-open-light.jpg '5-burger-menu-open-light')
+
+![5-burger-menu-open-dark](/5-burger-menu-open-dark.jpg '5-burger-menu-open-dark')
+
+![6-single-article-light](/6-single-article-light.jpg '6-single-article-light')
+
+![6-single-article-dark](/6-single-article-dark.jpg '6-single-article-dark')
+
+![7-single-article-responsive-dark](/7-single-article-responsive-dark.jpg '7-single-article-responsive-dark')
+
+![8-comments-open-light](/8-comments-open-light.jpg '8-comments-open-light')
+
+![8-comments-open-dark](/8-comments-open-dark.jpg '8-comments-open-dark')
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { getArticle } from '../utils/api';
+import { getArticle, patchArticleVotes } from '../utils/api';
 import Comments from './Comments';
 
 export default function Article() {
@@ -9,6 +9,7 @@ export default function Article() {
 	const [isLoading, setIsLoading] = useState(true);
 	const [isOpen, setIsOpen] = useState(false);
 	const [buttonText, setButtonText] = useState('Show Comments');
+	const [articleVotes, setArticleVotes] = useState();
 
 	function ShowComments() {
 		isOpen ? setIsOpen(false) : setIsOpen(true);
@@ -21,6 +22,24 @@ export default function Article() {
 			setArticle(article);
 		});
 	}, [article_id]);
+
+	const increaseVotes = () => {
+		setArticleVotes(article.votes);
+		article.votes++;
+		setArticleVotes(article.votes);
+		patchArticleVotes(article_id, +1).catch((err) => {
+			console.log('Something went wrong - please try again');
+		});
+	};
+	const decreaseVotes = () => {
+		setArticleVotes(article.votes);
+		article.votes--;
+		setArticleVotes(article.votes);
+		patchArticleVotes(article_id, -1).catch((err) => {
+			console.log('Something went wrong - please try again');
+		});
+	};
+
 	return (
 		<>
 			<h2>{article.title}</h2>
@@ -33,16 +52,22 @@ export default function Article() {
 							<Link className='topic-link' to={`/articles/topics/${article.slug}`}>
 								<h3>{article.topic}</h3>
 							</Link>
-							<img className='article-img' src={article.img_url} alt={article.title} />
-							<p className='article-body'>{article.body}</p>
+							<div className='img-and-text-div'>
+								<img className='article-img' src={article.img_url} alt={article.title} />
+								<p className='article-body'>{article.body}</p>
+							</div>
 							<h4>Author: {article.author}</h4>
 							<div className='article-date-div'>
 								<p className='article-date-p'>Date: {article.created_at.substring(0, 10)}</p>
 								<div className='article-votes-counter-div'>
 									<p>Likes:</p>
-									<span className='material-symbols-outlined'>thumb_up</span>
+									<button onClick={() => increaseVotes()} className='material-symbols-outlined'>
+										thumb_up
+									</button>
 									<span className='article-votes-counter'>{article.votes}</span>
-									<span className='material-symbols-outlined'>thumb_down</span>
+									<button onClick={() => decreaseVotes()} className='material-symbols-outlined'>
+										thumb_down
+									</button>
 								</div>
 							</div>
 							<button className='show-comments-button' onClick={ShowComments}>
